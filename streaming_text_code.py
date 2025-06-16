46371145b0b2bcf3322dea8b20f5a7ff6592d501
mustafa861 <mustafa1212adnan@gmail.com>
2025-06-16 21:46:59 +0500

    .

---
+++ streaming_text_code.py
@@ -0,0 +1,51 @@
+import os
+from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel
+
+
+from agents import (
+    Agent,
+    Runner,
+    set_default_openai_api,
+    set_default_openai_client,
+    set_tracing_disabled,
+)
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+
+external_client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash",
+    openai_client=external_client
+)
+
+set_default_openai_client(client=external_client, use_for_tracing=False)
+set_default_openai_api("chat_completions")
+set_tracing_disabled(disabled=True)
+
+
+import asyncio
+
+from openai.types.responses import ResponseTextDeltaEvent
+
+from agents import Agent, Runner
+
+
+async def main():
+    agent = Agent(
+        name="Joker",
+        instructions="You are a helpful assistant.",
+        model=model
+    )
+
+    result = Runner.run_streamed(agent, input="give me 500 words of article.")
+    async for event in result.stream_events():
+        if event.type == "raw_response_event" and isinstance(event.data, ResponseTextDeltaEvent):
+            print(event.data.delta, end="", flush=True)
+
+
+
+asyncio.run(main())
