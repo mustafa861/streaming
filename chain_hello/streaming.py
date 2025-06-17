ae620765f6636de2c2a8d9be69a36869e8e396eb
mustafa861 <mustafa1212adnan@gmail.com>
2025-06-17 20:24:28 +0500

    .

--- chain_hello/streaming.py
+++ chain_hello/streaming.py
@@ -1,6 +1,6 @@
 import os
 from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel
-
+from agents.run import RunConfig
 
 from agents import (
     Agent,
@@ -9,7 +9,9 @@ from agents import (
     set_default_openai_client,
     set_tracing_disabled,
 )
-gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+gemini_api_key = os.getenv('GEMINI_API_KEY')
+
 
 
 external_client = AsyncOpenAI(
@@ -26,13 +28,11 @@ set_default_openai_client(client=external_client, use_for_tracing=False)
 set_default_openai_api("chat_completions")
 set_tracing_disabled(disabled=True)
 
-
 import asyncio
 
 from openai.types.responses import ResponseTextDeltaEvent
 
 from agents import Agent, Runner
-
 
 async def main():
     agent = Agent(
@@ -41,11 +41,9 @@ async def main():
         model=model
     )
 
-    result = Runner.run_streamed(agent, input="give me 500 words of article.")
+    result = Runner.run_streamed(agent, input="Please tell me 5 jokes.")
     async for event in result.stream_events():
         if event.type == "raw_response_event" and isinstance(event.data, ResponseTextDeltaEvent):
             print(event.data.delta, end="", flush=True)
-
-
 
 asyncio.run(main())
